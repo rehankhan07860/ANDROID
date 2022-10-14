07f0e8c36212766fdda341e04a8487fe2eed9e45
Rehan Kahn <90464921+rehankhan07860@users.noreply.github.com>
2022-10-14 17:26:57 +0530

    Add files via upload

---
+++ src/main/java/com/example/shareapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.shareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        textView = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Insert subject here");
+                String app_url = " https://play.google.com/store/apps/details?id=my.example.javatpoint";
+                shareIntent.putExtra(Intent.EXTRA_TEXT,app_url);
+                startActivity(Intent.createChooser(shareIntent,"Shar via"));
+
+            }
+        });
+    }
+}
